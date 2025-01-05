1c7ca9af54fd8eb992a106df20a82521cd70cf8f
J1mryce <165799112+J1mryce@users.noreply.github.com>
2025-01-05 23:54:16 +0100

    Delete site bioviz part 3 mehdi/site bioviz part 2 directory

--- site bioviz part 3 mehdi/site bioviz part 2/javascript/graphique.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('graph-selector').addEventListener('change', function (e) {
-  hideAll();
-  const selectedGraph = e.target.value;
-  document.getElementById(selectedGraph).style.display = 'block';
-  document.querySelector(`#titre${selectedGraph[selectedGraph.length - 1]}`).style.display = 'block';
-});
